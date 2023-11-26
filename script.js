70b6712cac52be811945bbf56b8d0e3945976912
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-26 22:33:56 +0100

    problema risolto, posizionamento css migliorato

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const seconds = document.querySelector('span#seconds');
 
 // data dove far finire il countdown
 const countdownEnd = new Date(2024, 0, 1, 0, 0, 0, 0);
-console.log(dataFinale);
 
 const timer = setInterval(function(){
     // orario attuale
