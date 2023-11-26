dcf66b14eee53525904d1ca585a71a5bbb5f0d09
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-26 22:08:40 +0100

    js variabili collegate ai rispettivi span

---
+++ script.js
@@ -0,0 +1,5 @@
+// faccio 4 variabili e le collego ai rispettivi span
+const days = document.querySelector('span#days');
+const hours = document.querySelector('span#hours');
+const minutes = document.querySelector('span#minutes');
+const seconds = document.querySelector('span#seconds');
