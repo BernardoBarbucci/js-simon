ae30d5af03502dd87f4bd56f412ced1d6c0af896
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-26 22:18:11 +0100

    data dove far finire il countdown

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const days = document.querySelector('span#days');
 const hours = document.querySelector('span#hours');
 const minutes = document.querySelector('span#minutes');
 const seconds = document.querySelector('span#seconds');
+
+// data dove far finire il countdown
+const countdownEnd = new Date(2024, 0, 1, 0, 0, 0, 0);
+console.log(dataFinale);
